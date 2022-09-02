c12428343836fabcc3d94996c3ae661df01b13d1
mario_amazing <marian1amazing@gmail.com>
2022-09-02 14:40:17 +0400

    Add bulma

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import './App.css';
+import Bulma from '@vizuaalog/bulmajs';
+
+import 'bulma/css/bulma.min.css';
 
 function App() {
+
+  const alert = function() {
+    Bulma().alert({
+      type: 'success',
+      title: 'В жопу тебя раз!',
+      body: '👉👌',
+      confirm: {
+        label: 'Повторить!',
+        onClick: function() {
+          Bulma().alert({
+            type: 'success',
+            title: 'В жопу тебя два!',
+            body: '👉👉👌',
+            confirm: {
+              label: 'Повторить!',
+              onClick: function() {
+                Bulma().alert({
+                  title: 'Ну все, хватит с тебя',
+                  body: '🌚🌝',
+                  confirm: 'Я попозже зайду!',
+                });
+              },
+            },
+            cancel: 'Ок :)'
+          })
+        },
+      },
+      cancel: 'Ок :)'
+    })
+  }
   return (
-    <button onClick={() => {if(window.confirm('Are you sure to delete this record?')){hhhhh }}}></button>
+    <div class="content has-text-centered pt-1">
+      <button className='button is-large content has-text-centered' onClick={alert}>Нуу нажмииииии на меня</button>
+    </div>
   );
 }
 
